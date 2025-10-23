e01655149caa1579b836af381ec5c283843b6640
Jennifer Román <jenniferromanmuerte@gmail.com>
2025-10-23 19:21:17 +0200

    Finish design card in preview and create event in main for select design

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,36 @@
 "use strict";
 
-console.log(">> Ready :)");
+const magazineDesign = document.querySelector("#magazineDesign");
+const economyDesign = document.querySelector("#economyDesign");
+const newspaperDesign = document.querySelector("#newspaperDesign")
+const card = document.querySelector(".preview__article");
+const ImgPreview = document.querySelector(
+  ".preview__article--div--containerImg--img"
+);
+
+const addStyle = (style) => {
+  console.log(`La funcion de cambio de clases está entrando y recibo ${style}`)
+  card.classList.forEach((className) => {
+    if (className !== "preview__article") {
+      card.classList.remove(className);
+    }
+  });
+
+  if (style === "type-sensacionalist") {
+    card.classList.add(style);
+    ImgPreview.src = "/images/background_sensacionalist.png";
+  }
+  if (style === "type-economist") {
+    card.classList.add(style);
+    ImgPreview.src = "/images/img_preview_economy.png";
+  }
+  if (style === "type-generalist") {
+    card.classList.add(style);
+    ImgPreview.src = "/images/img_preview_generalist.png";
+  }
+};
+
+magazineDesign.addEventListener("click", () => addStyle("type-sensacionalist"));
+economyDesign.addEventListener("click", () => addStyle("type-economist"));
+newspaperDesign.addEventListener("click", () => addStyle("type-generalist"));
+
